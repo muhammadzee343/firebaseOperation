76e590cfbb9f9023b064cd6fe25a4aa3c8465cef
muhammadzee343 <muhammad.zee343@gmail.com>
2020-07-23 12:40:06 +0500

    fresh project

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Abc</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBXWhhw6HsoiN0-jiqXePUCdg1tZHOlxvs',
+  authDomain: 'fir-operation-290ae.firebaseapp.com',
+  databaseURL: 'https://fir-operation-290ae.firebaseio.com',
+  projectId: 'fir-operation-290ae',
+  storageBucket: 'fir-operation-290ae.appspot.com',
+  messagingSenderId: '946723658812',
+  appId: '1:946723658812:web:723ca2c95e0153948ef739',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
