aaa4953127209bd089f3c5795c48088847f18724
muhammadzee343 <muhammad.zee343@gmail.com>
2020-07-23 12:54:31 +0500

    post data into firebase

--- App.js
+++ App.js
@@ -7,21 +7,66 @@
  */
 
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {firebaseConfig} from './config';
-import {Item, Input, Label} from 'native-base';
+import firebase from 'firebase';
+import {Item, Input, Label, Button} from 'native-base';
 
 class App extends Component {
+  state = {
+    stateText: '',
+    myList: [],
+  };
+
+  saveitem() {
+    const manualAddChild = firebase.database().ref('wishes');
+    manualAddChild.push({
+      text: this.state.stateText,
+      time: Date.now(),
+    });
+  }
+
   render() {
     return (
       <View>
         <Item floatingLabel>
           <Label>Username</Label>
-          <Input />
+          <Input
+            value={this.state.stateText}
+            onChangeText={(text) => this.setState({stateText: text})}
+          />
         </Item>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            padding: 20,
+          }}>
+          <Button
+            rounded
+            success
+            style={styles.btnstyle}
+            onPress={() => this.saveitem()}>
+            <Text style={styles.textstyle}>Add</Text>
+          </Button>
+          <Button danger rounded style={styles.btnstyle}>
+            <Text style={styles.textstyle}>Delete All</Text>
+          </Button>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  btnstyle: {
+    width: 160,
+    padding: 15,
+    justifyContent: 'center',
+  },
+  textstyle: {
+    fontSize: 20,
+  },
+});
 
 export default App;
